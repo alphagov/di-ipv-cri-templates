cd4e0e412ad1526c56cb65dea42f103077199eae
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-07-31 17:13:08 +0100

    OJ-1766: Refactoring and added deploy.sh

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -1,26 +1,4 @@
 export default async function (plop) {
-  plop.setGenerator("stack:new", {
-    description: "General configuration for creating a stack",
-    prompts: [
-      {
-        type: "input",
-        name: "stackName",
-        message: "Name of Stack?",
-      },
-    ],
-    actions: [
-      {
-        type: "addMany",
-        destination: "./infrastructure",
-        base: "templates/infrastructure",
-        templateFiles: "**",
-        verbose: true,
-        force: false,
-        skipIfExists: true,
-      },
-    ],
-  });
-
   plop.setGenerator("lambda:new", {
     description: "General configuration for lambdas",
     prompts: [
@@ -44,13 +22,13 @@ export default async function (plop) {
         type: "append",
         path: "./infrastructure/template.yaml",
         pattern: /(Resources:)/,
-        templateFile: "./templates/hbs-files/lambda-resource.hbs",
+        templateFile: "./templates/infrastructure/resource-fragment.hbs",
       },
       {
         type: "append",
         path: "./infrastructure/template.yaml",
         pattern: /(Outputs:)/,
-        templateFile: "./templates/hbs-files/lambda-outputs.hbs",
+        templateFile: "./templates/infrastructure/output-fragment.hbs",
       },
     ],
   });
