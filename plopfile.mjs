898c6386a3cbc2251c307045cd8325033c453f9d
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2023-09-12 17:29:04 +0100

    chore: Sort generators

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,6 +1,6 @@
 export default async function (plop) {
-  await plop.load("./repo/index.mjs");
-  await plop.load("./lambda/index.mjs");
   await plop.load("./api/index.mjs");
   await plop.load("./frontend/index.mjs");
+  await plop.load("./lambda/index.mjs");
+  await plop.load("./repo/index.mjs");
 }
