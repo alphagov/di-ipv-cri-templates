2db7b489a63479812e36b602be1e392b02f60835
Karol C. Gancarz <karol.sinx@gmail.com>
2023-12-11 12:05:12 +0000

    BAU: Update the dev deployment script
    - Apply formatting
    - Add tags to deployed stacks
    - Use an S3 prefix
    - Use the region from the environment, or use the default value if it's not set
    - Set the -u flag to fail if any variable is missing a value
    - Don't run `./gradlew clean` since this template is for TypeScript lambdas
    - Remove unused parameter overrides
    - Add an execution permission to the deploy script
      Ran `chmod +x api/templates/deploy.sh`
    
    - Enable easy deployment to dev
    
      - Derive stack name from the user's name and the CRI name if the name is not provided to the script
    
      - Add a deploy script to the package.json files so a stack can be quickly deployed from anywhere in the repo by simply running `npm run deploy`

--- api/index.mjs
+++ api/index.mjs
@@ -1,29 +1,30 @@
+import path from "path";
+
 export default async function (plop) {
   plop.setGenerator("api:init", {
-    prompts: [
-      {
-        type: "input",
-        name: "criName",
-        message: "CRI Name?",
-      },
-    ],
-    actions: [
-      {
-        type: "addMany",
-        destination: "./infrastructure",
-        base: "templates/infrastructure",
-        templateFiles: "**",
-        verbose: true,
-        force: false,
-        skipIfExists: true,
-      },
-      {
-        type: "add",
-        path: "deploy.sh",
-        templateFile: "templates/deploy.sh.hbs",
-        verbose: true,
-        skipIfExists: true,
-      },
-    ],
+    prompts: [],
+    actions: function (data) {
+      data.criName = path.basename(plop.getDestBasePath());
+      data.criNameShort = data.criName.replace(/^ipv-cri-/, "");
+
+      return [
+        {
+          type: "addMany",
+          destination: "./infrastructure",
+          base: "templates/infrastructure",
+          templateFiles: "**",
+          verbose: true,
+          force: false,
+          skipIfExists: true,
+        },
+        {
+          type: "add",
+          path: "deploy.sh",
+          templateFile: "templates/deploy.sh.hbs",
+          verbose: true,
+          force: true,
+        },
+      ];
+    },
   });
 }
